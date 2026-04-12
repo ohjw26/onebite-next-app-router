e42a12edb3335ea66e42e1b15446211bfe327cf6
Jiwon Oh <ohjiwon3040@gmail.com>
2026-04-12 23:12:17 +0900

    Next.js streaming and error handling

--- section04/src/app/(with-searchbar)/page.tsx
+++ section04/src/app/(with-searchbar)/page.tsx
@@ -1,6 +1,9 @@
 import BookItem from "@/components/book-item";
 import style from "./page.module.css";
 import { BookData } from "@/types";
+import { delay } from "@/util/delay";
+import { Suspense } from "react";
+import BookItemSkeleton from "@/components/skeleton/book-item-skeleton";
 
 //export const dynamic = "force-dynamic";
 //특정 페이지의 유형을 강제로 Static, Dynamic 페이지로 설정
@@ -10,6 +13,7 @@ import { BookData } from "@/types";
 // 4. error : 페이지를 강제로 Static 페이지로 설정 (설정하면 안되는 이유 -> 빌드 오류)
 
 async function AllBooks() {
+  await delay(1500);
   const response = await fetch(
     `${process.env.NEXT_PUBLIC_API_SERVER_URL}/book`,
     { cache: "force-cache" }
@@ -29,6 +33,7 @@ async function AllBooks() {
 }
 
 async function RecoBooks() {
+  await delay(3000);
   const response = await fetch(
     `${process.env.NEXT_PUBLIC_API_SERVER_URL}/book/random`,
     { next: { revalidate: 3 } }
@@ -47,17 +52,39 @@ async function RecoBooks() {
   );
 }
 
+export const dynamic = "force-dynamic";
+
 export default function Home() {
 
   return (
     <div className={style.container}>
       <section>
         <h3>지금 추천하는 도서</h3>
+        <Suspense 
+          fallback={
+            <>
+              <BookItemSkeleton />
+              <BookItemSkeleton />
+              <BookItemSkeleton />
+            </>
+          }
+        >
           <RecoBooks />
+        </Suspense>
       </section>
       <section>
         <h3>등록된 모든 도서</h3>
+        <Suspense 
+          fallback={
+            <>
+              <BookItemSkeleton />
+              <BookItemSkeleton />
+              <BookItemSkeleton />
+            </>
+          }
+        >
           <AllBooks />
+        </Suspense>
       </section>
     </div>
   );
--- section04/src/app/(with-searchbar)/search/page.tsx
+++ section04/src/app/(with-searchbar)/search/page.tsx
@@ -1,12 +1,10 @@
 import BookItem from "@/components/book-item";
 import { BookData } from "@/types";
+import { delay } from "@/util/delay";
+import { Suspense } from "react";
 
-export default async function Page({
-  searchParams,
-}: {
-  searchParams: Promise<{ q?: string }>;
-}) {
-  const { q } = await searchParams;
+async function SearchResult ({ q }: { q: string }) {
+  await delay(1500);
 
   const response = await fetch(
     `${process.env.NEXT_PUBLIC_API_SERVER_URL}/book/search?q=${q}`,
@@ -24,5 +22,21 @@ export default async function Page({
         <BookItem key={book.id} {...book} />
       ))}
     </div>
+  );
+}
+
+export default async function Page({
+  searchParams,
+}: {
+  searchParams: Promise<{ q?: string }>;
+}) {
+  const { q } = await searchParams;
+  return (
+    <Suspense 
+      key={q || ""}
+      fallback={<div>Loading ...</div>}
+    >
+      <SearchResult q={q || ""} />
+    </Suspense>
   );
 }
