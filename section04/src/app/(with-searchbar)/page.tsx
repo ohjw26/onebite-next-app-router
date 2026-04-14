f6f5a1f02c9f709c615754e506e3d1680bc17e4d
Jiwon Oh <ohjiwon3040@gmail.com>
2026-04-15 00:42:56 +0900

    SEO

--- section04/src/app/(with-searchbar)/page.tsx
+++ section04/src/app/(with-searchbar)/page.tsx
@@ -5,6 +5,7 @@ import { delay } from "@/util/delay";
 import { Suspense } from "react";
 import BookItemSkeleton from "@/components/skeleton/book-item-skeleton";
 import BookListSkeleton from "@/components/skeleton/book-list-skeleton";
+import { Metadata } from "next";
 
 //export const dynamic = "force-dynamic";
 //특정 페이지의 유형을 강제로 Static, Dynamic 페이지로 설정
@@ -54,6 +55,16 @@ async function RecoBooks() {
 }
 
 export const dynamic = "force-dynamic";
+
+export const metadata: Metadata = {
+  title: "한입 북스",
+  description: "한입 북스에 등록된 도서를 만나보세요",
+  openGraph: {
+    title: "한입 북스",
+    description: "한입 북스에 등록된 도서를 만나보세요",
+    images: ["/thumbnail.png"],
+  },
+};
 
 export default function Home() {
 
